e98765f7e60ed31f746fb78eb3679023e17fca06
zhangqinghua <the.patron.saint.of.science@gmail.com>
2017-12-25 02:59:58 +0800

    commit

--- src/main/java/com/mall/controller/GoodsController.java
+++ src/main/java/com/mall/controller/GoodsController.java
@@ -163,7 +163,7 @@ public class GoodsController {
     @RequestMapping(value = "/weixin_add", method = RequestMethod.POST)
     public String weixin_add(Goods goods) {
         goodsService.add(goods);
-        return "weixin/scan";
+        return "redirect:weixin/scan";
     }
 
 
